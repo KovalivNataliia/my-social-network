285efc1e85050bc9028689ffeead32dabe432496
NataliiaKovaliv <nataliiakovaliv95@gmail.com>
2021-06-16 16:41:28 +0300

    create dialogs component

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,11 @@
+import classNames from './Dialogs.module.css'
+
+const Dialogs = () => {
+    return (
+        <div className={classNames.content}>
+            <h2>Dialogs</h2>
+        </div>
+    );
+}
+
+export default Dialogs
