e3f50290c7e84c8df969c192ee6c5667f0640ef3
NataliiaKovaliv <nataliiakovaliv95@gmail.com>
2021-06-17 13:40:57 +0300

    react-router-dom

---
+++ src/components/MainPage/Settings/Settings.jsx
@@ -0,0 +1,11 @@
+import classNames from './Settings.module.css'
+
+const Settings = () => {
+    return (
+        <div className={classNames.content}>
+            <h2>Settings</h2>
+        </div>
+    );
+}
+
+export default Settings
